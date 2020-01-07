8ab956660cf83db19c1867df1596de7bbc3ab6c6
Kamil Szczepański <kamil.szczepanski@netguru.com>
2020-01-07 14:12:39 +0100

    Initial Commit

---
+++ GradientsAnimations/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  GradientsAnimations
+//
+//  Created by Kamil Szczepański on 07/01/2020.
+//  Copyright © 2020 Kamil Szczepański. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
